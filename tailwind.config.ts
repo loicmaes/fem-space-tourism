893ccd2d274eb8774586549ebdc9dae1fe4d63cb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-28 01:38:26 +0200

    :rocket: Setup tailwind css into app

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+export default {
+  content: [],
+  theme: {
+    colors: {
+      navy: '#0B0D17',
+      blue: '#D0D6F9',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      bellefair: 'Bellefair, serif',
+      barlow: '"Barlow Condensed", sans-serif',
+    },
+    plugins: [],
+  }
+}
